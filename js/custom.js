9d3dd32da0c9548a976b28b6aec443f3839e474f
unknown <biz.mastercreationz@gmail.com>
2018-11-21 11:25:25 +0530

    tulip changes

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+  $(document).ready(function(){
+            var submitIcon = $('.searchbox-icon');
+            var inputBox = $('.searchbox-input');
+            var searchBox = $('.searchbox');
+            var isOpen = false;
+            submitIcon.click(function(){
+                if(isOpen == false){
+                    searchBox.addClass('searchbox-open');
+                    inputBox.focus();
+                    isOpen = true;
+                } else {
+                    searchBox.removeClass('searchbox-open');
+                    inputBox.focusout();
+                    isOpen = false;
+                }
+            });  
+             submitIcon.mouseup(function(){
+                    return false;
+                });
+            searchBox.mouseup(function(){
+                    return false;
+                });
+            $(document).mouseup(function(){
+                    if(isOpen == true){
+                        $('.searchbox-icon').css('display','block');
+                        submitIcon.click();
+                    }
+                });
+        });
+            
+
+            $(document).ready(function(){
+                var mborder = '<span></span>';
+                $('.top-menu .navbar-nav li a').not('.micon a').after(mborder);
+             
+    $('.brand-link').click(function() { 
+        event.preventDefault();
+        $('.brand-section').toggle(); 
+    });
+
+
+     });   
